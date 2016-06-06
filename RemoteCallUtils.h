5abd0d81379d7d32d60088e3ff0538912c9b4068
amarmer <amarmer@users.noreply.github.com>
2016-06-05 22:40:52 -0700

    Add files via upload

---
+++ RemoteCallUtils.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <string>
+#include <sstream> 
+
+namespace RemoteCall
+{
+    template<typename C, typename Ret, typename ...Args>
+    auto MethodReturnType(Ret(C::*)(Args...)) -> Ret;
+
+    struct ToString: public std::stringstream
+    {
+        template <typename T>
+        ToString& operator << (const T& t)
+        {
+            ((std::stringstream&)*this) << t;
+
+            return *this;
+        }
+
+        operator std::string()
+        {
+            return ((std::stringstream&)(*this)).str();
+        }
+    };
+}
